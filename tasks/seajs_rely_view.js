fd782f687b8fb62b307b67aef9b4077e6f8d8cdd
hzmailin <lyonmai@126.com>
2014-09-26 17:21:08 +0800

    misc:project init

---
+++ tasks/seajs_rely_view.js
@@ -0,0 +1,50 @@
+/*
+ * seajs-rely-view
+ * https://github.com/yongwumai/seajs-rely-view
+ *
+ * Copyright (c) 2014 Len
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+module.exports = function(grunt) {
+
+  // Please see the Grunt documentation for more information regarding task
+  // creation: http://gruntjs.com/creating-tasks
+
+  grunt.registerMultiTask('seajs_rely_view', 'a tool checkout seajs module rely.', function() {
+    // Merge task-specific and/or target-specific options with these defaults.
+    var options = this.options({
+      punctuation: '.',
+      separator: ', '
+    });
+
+    // Iterate over all specified file groups.
+    this.files.forEach(function(f) {
+      // Concat specified files.
+      var src = f.src.filter(function(filepath) {
+        // Warn on and remove invalid source files (if nonull was set).
+        if (!grunt.file.exists(filepath)) {
+          grunt.log.warn('Source file "' + filepath + '" not found.');
+          return false;
+        } else {
+          return true;
+        }
+      }).map(function(filepath) {
+        // Read file source.
+        return grunt.file.read(filepath);
+      }).join(grunt.util.normalizelf(options.separator));
+
+      // Handle options.
+      src += options.punctuation;
+
+      // Write the destination file.
+      grunt.file.write(f.dest, src);
+
+      // Print a success message.
+      grunt.log.writeln('File "' + f.dest + '" created.');
+    });
+  });
+
+};
